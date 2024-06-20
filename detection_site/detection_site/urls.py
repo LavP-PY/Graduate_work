9e02098d36d75a16e4c6abeb0f573d48e11f0d32
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-06-20 23:27:33 +0300

    Update urls.py

--- detection_site/detection_site/urls.py
+++ detection_site/detection_site/urls.py
@@ -26,11 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('object_detection/', include('object_detection.urls')),
     path('', RedirectView.as_view(url='/object_detection/', permanent=True)),
-    # path('password-reset/', password_reset, name='password_reset'),
-    # path('password-reset/done/', password_reset_done, name='password_reset_done'),
-    #
-    # path('reset/<uidb64>/<token>/', password_reset_confirm, name='password_reset_confirm'),
-    # # path('password_reset_confirm/', password_reset_confirm, name='password_reset_confirm'),
-    #
-    # path('reset/done/', password_reset_complete, name='password_reset_complete')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
