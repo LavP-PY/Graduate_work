c82501ea1f9c4add1bd625091e7925b144f87368
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-06-26 21:46:13 +0300

    admin.py

--- detection_site/object_detection/admin.py
+++ detection_site/object_detection/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import ImageFeed, DetectedObject
 
+# Регистрация моделей для отображения в административной панели Django
 admin.site.register(ImageFeed)
 admin.site.register(DetectedObject)
