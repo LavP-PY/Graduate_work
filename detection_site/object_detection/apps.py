4ef2e95d2baf5b50404af6dc468eca5e24491332
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-06-26 21:46:38 +0300

    apps.py

--- detection_site/object_detection/apps.py
+++ detection_site/object_detection/apps.py
@@ -1,6 +1,17 @@
+# Модуль отвечает за конфигурацию приложения Django.
+# Он предоставляет метаданные для приложения `object_detection`, такие, как название приложения,
+# основной класс конфигурации и другие настройки, связанные с приложением в контексте Django
+
 from django.apps import AppConfig
 
 
 class ObjectDetectionConfig(AppConfig):
+    """
+    Конфигурация приложения object_detection для Django.
+
+    Attributes:
+        default_auto_field (str): Поле, автоматически создаваемое для новых моделей.
+        name (str): Имя приложения, используется для идентификации в Django.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'object_detection'
